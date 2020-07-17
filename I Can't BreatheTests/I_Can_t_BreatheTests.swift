65d91d54da9c15f96ff041e64b655cc52850188b
shanicebrown <sbrown30@csu.edu>
2020-07-17 10:07:13 -0500

    This is a one page app

---
+++ I Can't BreatheTests/I_Can_t_BreatheTests.swift
@@ -0,0 +1,34 @@
+//
+//  I_Can_t_BreatheTests.swift
+//  I Can't BreatheTests
+//
+//  Created by admin on 7/8/20.
+//  Copyright © 2020 Shanice Brown. All rights reserved.
+//
+
+import XCTest
+@testable import I_Can_t_Breathe
+
+class I_Can_t_BreatheTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
